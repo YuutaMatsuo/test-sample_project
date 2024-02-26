dc46d5ebbf7cad17662a9ea9557113b104625d35
kuro115 <126228724+YuutaMatsuo@users.noreply.github.com>
2024-02-26 23:20:33 +0900

    テスト

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,12 +2,16 @@ package sample;
 
 public class Main {
 	public static void main(String[] args) {
-	hello();
-	
+	hello("みなと");
+	purasu(10,15);
 	
 	}
 	
-	public static void hello() {
-		System.out.println("こんにちは");
+	public static void hello(String name) {
+		System.out.println(name + "さん、こんにちは");
+	}
+	
+	public static void purasu(int a, int b) {
+		System.out.println(a + b);
 	}
 }
